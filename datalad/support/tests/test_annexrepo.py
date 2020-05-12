9761b75c7d0cb1b07e98c5cc5392b43f41058924
Kyle Meyer <kyle@kyleam.com>
2020-05-12 13:24:36 -0400

    TST: annexrepo: Don't skip test_annex_ssh()
    We have no satisfying answer to why this is hanging with the
    localhost-targeting set up on Travis, but nobody has been able to
    trigger it outside of Travis, so let's see if it's still an issue if
    Travis is switched to using Docker containers as the SSH targets.

--- datalad/support/tests/test_annexrepo.py
+++ datalad/support/tests/test_annexrepo.py
@@ -1155,12 +1155,6 @@ def test_annex_ssh(repo_path, remote_1_path, remote_2_path):
     else:
         ok_(not exists(socket_1))
 
-    # TODO: figure it out
-    if external_versions['cmd:annex'] >= '8.20200226':
-        # This is not necessarily the version where it started to hang
-        # See https://github.com/datalad/datalad/pull/4265 for more info
-        raise SkipTest("Version of git-annex might cause us to stall.")
-
     # remote interaction causes socket to be created:
     try:
         # Note: For some reason, it hangs if log_stdout/err True
